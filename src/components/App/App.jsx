a6b0ba2278dd53615ba0c3fd782e90340482835c
Amanda Zimmerman <amanda.zimms@gmail.com>
2022-01-22 15:12:51 -0600

    removed extraneous components/ routes - now when a user is logged in it displays the 'today' page, otherwise it routes them to login/register page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -13,12 +13,11 @@ import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
-import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import Today from '../Today/Today';
 
 import './App.css';
 
@@ -34,79 +33,47 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        {/* If a user is logged in, show nav bar */}
+        {user.id &&
+          <Nav />
+        } 
         <Switch>
           {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
+          <Redirect exact from="/" to="/today" />
 
           {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/user will show the UserPage if the user is logged in.
+            Visiting localhost:3000/user will show the ProfilePage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/user"
-          >
-            <UserPage />
+        
+          {/* TODO uncomment when profile page is built */}
+          {/* logged in shows ProfilePage, else shows LoginPage */}
+          {/* <ProtectedRoute exact path="/profile">
+            <ProfilePage/>
+          </ProtectedRoute> */}
+
+          {/* logged in shows Today page, else shows LoginPage */}
+          <ProtectedRoute exact path="/today">
+            <Today />
           </ProtectedRoute>
 
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
-          </ProtectedRoute>
 
-          <Route
-            exact
-            path="/login"
-          >
+          <Route exact path="/login">
             {user.id ?
-              // If the user is already logged in, 
-              // redirect to the /user page
-              <Redirect to="/user" />
-              :
+              // If the user is already logged in, redirect to the /today page
+              <Redirect to="/today" /> :
               // Otherwise, show the login page
               <LoginPage />
             }
           </Route>
 
-          <Route
-            exact
-            path="/registration"
-          >
+          <Route exact path="/registration">
             {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
+              // If the user is already logged in, redirect them to the /today page
+              <Redirect to="/today" /> :
               // Otherwise, show the registration page
               <RegisterPage />
-            }
-          </Route>
-
-          <Route
-            exact
-            path="/home"
-          >
-            {user.id ?
-              // If the user is already logged in, 
-              // redirect them to the /user page
-              <Redirect to="/user" />
-              :
-              // Otherwise, show the Landing page
-              <LandingPage />
             }
           </Route>
 
@@ -114,10 +81,11 @@ function App() {
           <Route>
             <h1>404</h1>
           </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
   );
 }
 
---
+++ src/components/Today/Today.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import RegisterForm from '../RegisterForm/RegisterForm';
+
+function Today() {
+
+  return (
+    <div>
+      <h1>TODAY</h1>
+      <center>
+        <button>
+          Click Me
+        </button>
+      </center>
+    </div>
+  );
+}
+
+export default Today;
